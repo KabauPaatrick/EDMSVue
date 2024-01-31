27cf7e367c8a318cd8a79da2559db088e23a690c
Chege-Simon <petekim211@gmail.com>
2024-01-31 16:59:45 +0300

    endpoint update

--- src/main.js
+++ src/main.js
@@ -1,33 +1,33 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router.js'
 import "./assets/style.css"
-import 'bootstrap/dist/css/bootstrap.css'
+import router from './router.js'
 
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
+import 'sweetalert2/dist/sweetalert2.min.css'
+import VueSweetalert2 from 'vue-sweetalert2'
 
-import Toast, { POSITION } from "vue-toastification";
-import "vue-toastification/dist/index.css";
+import Toast, { POSITION } from "vue-toastification"
+import "vue-toastification/dist/index.css"
 
-import Vue3Progress from "vue3-progress";
+import Vue3Progress from "vue3-progress"
 
 
 const app = createApp(App);
 // app.config.globalProperties.baseApiUrl = 'http://127.0.0.1:8000';
-app.config.globalProperties.baseApiUrl = 'https://demo-openchs.bitz-itc.com/edms/'
+app.config.globalProperties.baseApiUrl = 'http://edms-enpoints.bitz-itc.com';
 
 app.use(VueSweetalert2);
 const toast_options = {
-    // You can set your default options here 
-    position: POSITION.BOTTOM_RIGHT
+  // You can set your default options here 
+  position: POSITION.BOTTOM_RIGHT
 };
 app.use(Toast, toast_options);
 const progress_options = {
-    position: "fixed",
-    height: "4px",
-    color: '#355b11',
-  };
+  position: "fixed",
+  height: "4px",
+  color: '#355b11',
+};
 app.use(Vue3Progress, progress_options)
 app.use(router);
 app.mount('#app');
